0964fef2df01ef1716eb567a62c3ad3ab51fda3c
Glauro Juliani <glauro.juliani@sympla.com.br>
2017-07-20 11:48:56 -0300

    Login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,12 +1,43 @@
 <template lang="html">
 	<div>
-		<h2 class="md-title">Login</h2>
+		<div class="row">
+			<div class="col-xs-12">
+				<h2 class="md-title">Login</h2>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-4">
+				<form>
+					<div class="form-group">
+						<label for="exampleInputEmail1">Email address</label>
+						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" v-model="email">
+					</div>
+					<div class="form-group">
+						<label for="exampleInputPassword1">Password</label>
+						<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" v-model="password">
+					</div>
+					<button type="submit" class="btn btn-success" v-on:click="sendLogin()">Submit</button>
+				</form>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
 	export default {
-		name: 'login'
+		name: 'login',
+		data () {
+			return {
+				email: '',
+				password: ''
+			}
+		},
+		methods: {
+			sendLogin: function() {
+				var vm = this;
+				console.log(vm.email, vm.password)
+			}
+		}
 	}
 </script>
 
