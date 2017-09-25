27818ad5c2aa2c484aa680502ff6f8125e2451c0
glauroqj <glauro.juliani@hotmail.com>
2017-09-25 01:10:07 -0300

    add: mult-upload images / pendece: remove all images on component after upload complete

--- src/components/PanelSkills.vue
+++ src/components/PanelSkills.vue
@@ -23,10 +23,16 @@
 						</div>
 					</div>
 
-					<div class="form-group" v-if="urlImage == ''">
+					<div class="form-group">
 						<label for="" class="col-lg-2 control-label">Inserir IMG</label>
-						<div class="col-lg-8">
+<!-- 						<div class="col-lg-8">
 							<input type="file" class="form-control" id="upImage" v-on:change="loadImage">
+						</div> -->
+						<div class="col-lg-8">
+							<dropzone id="myVueDropzone" url="https://httpbin.org/post" v-on:vdropzone-success="uploadSuccess">
+								<!-- Optional parameters if any! -->
+								<input type="hidden" name="token" value="xxx">
+							</dropzone>
 						</div>
 						<div class="col-xs-2">
 							<div v-if="loadingImg==true">
@@ -41,13 +47,17 @@
 						</div>
 					</div>
 
-					<div class="box-preview-img form-group" v-if="urlImage != ''">
+					<div class="box-preview-img form-group" v-if="images.length != ''">
 						<label for="" class="col-lg-2 control-label">Preview Imagem</label>
 						<div class="col-lg-8">
-							<img :src="urlImage" alt="" class="img-circle" style="height: 30px; width: auto">
-							<button type="button" :pathUrl="pathUrl" v-on:click="pathUrl = pathUrl" class="btn btn-xs btn-danger" data-toggle="modal" data-target="#removeImg">
-								<i class="fa fa-trash" aria-hidden="true"></i>
-							</button>
+							<ul class="list-inline">
+								<li v-for="(image,index) in images">
+									<img :src="image.src" alt="" class="img-circle" style="height: 30px; width: auto">
+									<button type="button" :numimage="index" :pathUrl="image.path" v-on:click="pathUrl = image.path, numimage = index" class="btn btn-xs btn-danger" data-toggle="modal" data-target="#removeImg">
+										<i class="fa fa-trash" aria-hidden="true"></i>
+									</button>
+								</li>
+							</ul>
 						</div>
 					</div>
 
@@ -130,7 +140,7 @@
 					</div>
 					<div class="modal-footer">
 						<button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Não</button>
-						<button type="button" :pathUrl="pathUrl" class="btn btn-danger btn-sm" v-on:click="removeImg(pathUrl)" data-dismiss="modal" aria-label="Close">Desejo remover</button>
+						<button type="button" :pathUrl="pathUrl" :numimage="numimage" class="btn btn-danger btn-sm" v-on:click="removeImg(pathUrl, numimage)" data-dismiss="modal" aria-label="Close">Desejo remover</button>
 					</div>
 				</div>
 			</div>
@@ -140,8 +150,13 @@
 </template>
 
 <script>
+ /*
+	multi upload on firebase
+	https://gist.github.com/mcdonamp/d4f1327b58ad69334ef06327184df790
+	*/
 	import Firebase from 'firebase'
 	import {config} from '../firebase.js'
+	import Dropzone from 'vue2-dropzone'
 	import loading from '../components/Loading.vue'
 
 	export default {
@@ -149,21 +164,23 @@
 		data() {
 			return {
 				languages: [],
+				images: [],
 				languageType: '',
-				urlImage: '',
 				edit: '',
 				editKey: '',
 				loading: true,
 				loadingImg: false,
 				urlImage: '',
 				pathUrl: '',
+				numimage: '',
 				porcentagem: '',
 				bucket: '',
 				infos: {}
 			}
 		},
 		components:{
-			'loading': loading
+			'loading': loading,
+			'Dropzone': Dropzone
 		},
 		created() {
 			const storageRef = Firebase.storage().ref();
@@ -183,9 +200,15 @@
 				this.urlImage = '';
 				this.porcentagem = '';
 			},
+			uploadSuccess: function(e) {
+				/* called a function to upload firebase */
+				this.loadImage(e)
+			},
 			loadImage: function(e) {
 				var vm = this;
-				let file = e.target.files[0] || e.dataTransfer.files[0];
+				console.log(e)
+				// let file = e.target.files[0] || e.dataTransfer.files[0];
+				let file = e;
 				let uploadTask = this.bucket.child('skills/'+file.name).put(file);
 				uploadTask.on('state_changed', function(snapshot) {
 					console.log(snapshot)
@@ -196,22 +219,22 @@
 					vm.$toasted.show('Erro no upload :(');
 				}, function() {
 					vm.urlImage = uploadTask.snapshot.downloadURL;
+					let infoIMG = { src: vm.urlImage, path: vm.pathUrl }
+					vm.images.push(infoIMG);
 					vm.loadingImg = false;
 					vm.$toasted.show('Upload completo!');
 					$('#upImage').val('');
 				});
 			},
-			removeImg: function(path) {
+			removeImg: function(path, index) {
+				console.log(path, index)
 				var vm = this;
 				let deleteTask = this.bucket.child(path)
 				deleteTask.delete().then(function() {
 					vm.$toasted.show('Imagem deletada!');
-					vm.urlImage = '';
-					vm.pathUrl = '';
+					vm.images.splice(index, 1);
 				}).catch(function(error) {
 					vm.$toasted.show('Erro na remoção da imagem! :(');
-					vm.urlImage = '';
-					vm.pathUrl = '';
 				});
 			},
 			insertLanguage: function () {
@@ -222,7 +245,7 @@
 					urlImage: this.urlImage
 				}
 				array.push(data)
-				this.reset();
+				// this.reset();
 			},
 			removeLanguage: function (index, path) {
 				let array = this.languages;
@@ -292,10 +315,10 @@
 			}
 		}
 	}
-</script>
+	</script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+	<!-- Add "scoped" attribute to limit CSS to this component only -->
+	<style scoped>
 	.box-list {
 
 	}
@@ -311,4 +334,4 @@
 		margin-left: 5px;
 		font-size: 14px;
 	}
-</style>
+	</style>
